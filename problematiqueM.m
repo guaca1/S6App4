c63b642d432b7d8f5e3f3685e192e8866a009440
Felix Landry <flandryforget@ideoconcepts.ca>
2018-06-26 17:48:21 -0400

    Anal

--- problematiqueM.m
+++ problematiqueM.m
@@ -7,9 +7,12 @@ gainCapteur = 12./max(abs(SortieCapteur(:,2)));
 
 
 Pmoy = sum((gainCapteur*SortieCapteur(:,2)).^2)/length(SortieCapteur);
+PmoyCanalAnal = sum((entreeCanalAnal(1:64000)).^2)/length(entreeCanalAnal(1:64000));
 dS = sqrt(Pmoy*12/10^4.5);
 niveau = 24/dS;
 N = ceil(log2(niveau))
 y=audioplayer(AudioOut/max(AudioOut), 16000);
-histogram(ADCOut);
+%histogram(ADCOut);
+yanal = audioplayer(AudioOutAnal/max(AudioOutAnal), 16000)
 
+%10*log10(sum(signalAnal(1:64000).^2)/sum(bruitAnal(1:64000).^2))
